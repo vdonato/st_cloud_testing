52de16c4d2222219e79a681a797db94813a89da8
Vincent Donato <vincent@streamlit.io>
2021-12-17 13:38:06 -0500

    Write an app for intentionally using too much memory

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-st.header("Hello World")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
-st.header("Header")
+if "i" not in st.session_state:
+    st.session_state.i = 0
+
+
+def on_click():
+    twenty_million = 320 * 1_000_000
+    st.session_state[st.session_state.i] = "A" + (twenty_million * "H")
+    st.session_state.i += 1
+
+
+approximate_memory_usage = len(str(st.session_state)) / 1e6
+st.write(f"memory used by session state ~{approximate_memory_usage}MB")
+
+st.button("use more memory!", on_click=on_click)
