cc6186893b7ba2b8a86211427bc65e3d7324939d
Vincent Donato <vincent@streamlit.io>
2021-12-29 16:17:30 -0500

    Remove MPA testing files and replace app with just a header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,3 @@
 import streamlit as st
 
-if "i" not in st.session_state:
-    st.session_state.i = 0
-
-
-def on_click():
-    one_hundred_megabytes = 100 * 1_000_000
-    st.session_state[st.session_state.i] = "A" + (one_hundred_megabytes * "H")
-    st.session_state.i += 1
-
-
-approximate_memory_usage = len(str(st.session_state)) / 1e6
-st.write(f"memory used by session state ~{approximate_memory_usage}MB")
-
-st.button("use more memory!", on_click=on_click)
+st.header("Hello world")
