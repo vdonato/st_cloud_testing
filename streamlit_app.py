fa1a9bd95c1346f6a96097e189b378094bc644c2
Vincent Donato <vincent@streamlit.io>
2022-03-15 14:13:39 -0700

    Try installing cv2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,21 @@
+import cv2
+
 import streamlit as st
 
-st.header("Hello world!")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.header("Main page content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
+st.sidebar.write("# Sidebar content")
