50fc700d9bed3f015a42c3e9506b1eb25655b8a5
Vincent Donato <vincent.donato@snowflake.com>
2022-05-10 16:52:13 -0700

    Set log level to debug and print it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,6 @@
-import cv2
-
 import streamlit as st
 
+st.header("Main page content")
+
+st.write(f"logger.level: `{st.get_option('logger.level')}`")
 st.write(f"server.baseUrlPath: `{st.get_option('server.baseUrlPath')}`")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.header("Main page content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
-st.sidebar.write("# Sidebar content")
