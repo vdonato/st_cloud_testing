6d2e1ef98464863691165b36cce366eca23a5abf
Vincent Donato <vincent@streamlit.io>
2022-08-16 23:44:31 -0700

    Add pdf file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import pdfplumber
 import streamlit as st
 
-with pdfplumber.open(
-    "https://raw.githubusercontent.com/jsvine/pdfplumber/stable/examples/pdfs/background-checks.pdf"
-) as pdf:
+with pdfplumber.open("./background-checks.pdf") as pdf:
     img = pdf.pages[0].to_image(resolution=150)
     img.save("./as_image.png", format="PNG")
 
