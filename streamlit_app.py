4fd2d97c49bfa9de7915cc3a1620c28184c266d0
Vincent Donato <vincent@streamlit.io>
2023-03-11 19:09:05 -0800

    Update pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-if "counter" not in st.session_state:
-    st.session_state.counter = 0
-
-if st.button("increment"):
-    st.session_state.counter += 1
-
-st.write(f"count: {st.session_state.counter}")
+st.header("main page")
