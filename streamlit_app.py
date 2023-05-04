391618d57a1d63c64cad8d4f8fdf9ef43b1491c2
Vincent Donato <vincent@streamlit.io>
2023-05-03 17:41:33 -0700

    Use version 1.22.0 and add new test script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
+import requests
 import streamlit as st
 
-st.write(st.secrets.foo)
-st.write(st.secrets.bar)
-st.write(st.secrets.baz.qux)
-st.write(st.secrets.nonexistent)
+st.title(
+    "Streamlit gets stuck if session disconnects while waiting for HTTP response, and then tries to reconnect"
+)
+
+# Button that does something else
+if st.button("Another button"):
+    st.balloons()
+
+# Button that sends request
+if st.button("Send HTTP request"):
+    r = requests.post(url="https://httpstat.us/200?sleep=6000")
+    st.success("Request completed")
