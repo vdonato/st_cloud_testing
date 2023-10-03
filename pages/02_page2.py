ed3d2f022004ff1c9ade2bc823cc9a4ea95125c2
Vincent Donato <vincent@streamlit.io>
2023-10-03 15:22:58 -0700

    Remove streamlit version pin

--- pages/02_page2.py
+++ pages/02_page2.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from time import sleep
 
-st.header("page 2")
+st.write("starting")
+for i in range(5):
+    sleep(1)
+    st.write(i)
+st.write("finished")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,4 @@
-import requests
 import streamlit as st
 
-st.title(
-    "Streamlit gets stuck if session disconnects while waiting for HTTP response, and then tries to reconnect"
-)
-
-st.markdown(
-    """
-<a href="/page2#test" target="_self">page2</a>
-""",
-    unsafe_allow_html=True,
-)
-
-# Button that does something else
-if st.button("Another button"):
-    st.balloons()
-
-# Button that sends request
-if st.button("Send HTTP request"):
-    r = requests.post(url="https://httpstat.us/200?sleep=6000")
-    st.success("Request completed")
+conn = st.connection("snowpark")
+st.write(conn)
